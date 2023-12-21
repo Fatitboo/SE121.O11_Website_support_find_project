52b12deaae442c95b78fe997622153c2555ecb04
Fatitboo <21522448@gm.uit.edu.vn>
2023-12-21 18:32:24 +0700

    ,

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, HashRouter} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import {Provider} from 'react-redux'
@@ -8,8 +8,8 @@ import store from './redux/store/store.js'
 import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
 )
